068a198d231f71edfeeb35594404e7ec9c184165
Glenn Kinstler <glenn@glennkinstler.com>
2018-09-26 14:53:05 -0500

    refactoring with classes and methods

--- Sept20Sandbox/Program.cs
+++ Sept20Sandbox/Program.cs
@@ -5,39 +5,24 @@ namespace Sept20Sandbox
 {
     class Program
     {
+        public static Rooms ChooseRoom()
+        {
+            Console.WriteLine("Which room would you like to see?");
+            string answer1 = Console.ReadLine();
+            Rooms room1 = new Rooms();
+            room1.Name = answer1;
+            return room1;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello!  Welcome to our open house.");
 
-            List<string> rooms = new List<string>();
-            rooms.Add("kitchen");
-            rooms.Add("master bedroom");
-            rooms.Add("living room");
+            Rooms chosenRoom = ChooseRoom();
 
-            Console.WriteLine("What would you like to see first?  The kitchen?  The master bedroom?  The living room?");
-        
-            while (rooms.Count > 0)
-            {
-                string answer = Console.ReadLine();
-
-                foreach (string room in rooms)
-                {
-                    if (room == answer)
-                    {
-                        Console.WriteLine($"Great!  Let's to go {room}.\n");
-                        rooms.Remove(room);
-                    }
-
-                    Console.WriteLine("Which room would you like to see next?");
-                }
-            }
-
-
-            
-            
             Console.ReadLine();
 
-
+            //return a room to the method.  
 
                
 
---
+++ Sept20Sandbox/Rooms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sept20Sandbox
+{
+    class Rooms
+    {
+        public string Name { get; set; }
+        public string GoodLighting { get; set; }
+        public string NicePaint { get; set; }
+        public string RightSize { get; set; }
+    }
+}
