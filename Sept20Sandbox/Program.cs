7d34c7951624f34fa42a0a62f511c9d81423d58c
Glenn Kinstler <glenn@glennkinstler.com>
2018-10-01 09:32:34 -0500

    updated Oct. 1

--- Sept20Sandbox/Program.cs
+++ Sept20Sandbox/Program.cs
@@ -5,26 +5,36 @@ namespace Sept20Sandbox
 {
     class Program
     {
-        public static Rooms ChooseRoom()
-        {
-            Console.WriteLine("Which room would you like to see?");
+
+        static void Main()
+            {
+            Console.WriteLine("Hello!  Welcome to our open house.");
+
+            Console.WriteLine("Tell me three rooms that you would like to see.");
+
             string answer1 = Console.ReadLine();
             Rooms room1 = new Rooms();
             room1.Name = answer1;
-            return room1;
-        }
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello!  Welcome to our open house.");
+            Console.WriteLine("OK.  What else?");
 
-            Rooms chosenRoom = ChooseRoom();
+            string answer2 = Console.ReadLine();
+            Rooms room2 = new Rooms();
+            room2.Name = answer2;
+
+            Console.WriteLine("OK, and last?");
+
+            string answer3 = Console.ReadLine();
+            Rooms room3 = new Rooms();
+            room3.Name = answer3;
+
+            Console.WriteLine($"Great, we'll go see the {room1.Name}, the {room2.Name}, and the {room3.Name}.");
+
+
+
+            public static void RoomReview();
 
             Console.ReadLine();
-
-            //return a room to the method.  
-
-               
 
 
 
